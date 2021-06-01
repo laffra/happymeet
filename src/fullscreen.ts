f2d0faae9f024a701f90960dd90f3dfb6fcf7b87
laffra <laffra@gmail.com>
2021-06-01 14:10:57 +0200

    Avoid fullscreen when searching for an emoji

--- src/fullscreen.ts
+++ src/fullscreen.ts
@@ -1,5 +1,6 @@
 $("body")
     .on("keyup", event => {
+        if ($(event.target).hasClass("happymeet-emoji-filter")) return;
         switch (event.which) {
             case 70: // f
                 document.body.requestFullscreen();
